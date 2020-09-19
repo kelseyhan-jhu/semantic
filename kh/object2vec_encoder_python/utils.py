284a8b4fc27a237d558b0414b70c58052f6ff70b
kelseyhan-jhu <chan21@jhu.edu>
2020-09-19 07:30:26 -0400

    refactored analysis

--- kh/object2vec_encoder_python/utils.py
+++ kh/object2vec_encoder_python/utils.py
@@ -5,8 +5,9 @@ import scipy.io
 import torch
 from torchvision.transforms import functional as tr
 from sklearn.linear_model import Ridge, Lasso
+from sklearn.model_selection import KFold
+from sklearn.decomposition import PCA
 from collections import OrderedDict 
-
 
 # ImageNet mean and standard deviation. All images
 # passed to a PyTorch pre-trained model (e.g. AlexNet) must be
@@ -14,14 +15,51 @@ from collections import OrderedDict
 imagenet_mean = (0.485, 0.456, 0.406)
 imagenet_std = (0.229, 0.224, 0.225)
 
+def stack_features(path, fmri=True):
+    if fmri is True:
+        conditions = sorted(listdir(path))
+        condition_voxels = {}
+        for condition in conditions:
+            file_name = listdir((os.path.join(path, condition)))[0]
+            file_path = os.path.join(path, condition, file_name)
+            condition_voxels[condition] = np.load(file_path)
+        features_stacked = np.stack([condition_voxel for condition, condition_voxel in OrderedDict(condition_voxels).items()])
+        #print(np.shape(pred_voxels)) # should be 1470, 200
+    elif fmri is False: 
+        condition_features = torch.load(path)
+        features_stacked = np.stack([condition_feature for condition, condition_feature in OrderedDict(condition_features).items()])
+        #print(stacked_features.shape)
+    return features_stacked
+
+def cv_regression_w2s(features, w2s, fit=None, k=9, l2 = 0.0, pc=None):
+    if pc is not None:
+        pca = PCA(n_components=pc)
+    kf9 = KFold(n_splits = k)
+    rs = []
+    for train_index, test_index in kf9.split(features):
+        train_features = features[train_index,]
+        test_features =  features[test_index,]
+        if pc is not None:
+            pca.fit(train_features)    
+            train_features = pca.transform(train_features)
+            test_features = pca.transform(test_features)
+        train_embeddings = np.stack([embedding for i, embedding in enumerate(w2s.values()) if i in train_index])
+        test_embeddings = np.stack([embedding for i, embedding in enumerate(w2s.values()) if i in test_index])
+        _, r = regression(train_features, train_embeddings, test_features, test_embeddings, l2=l2)
+        rs.append(r)
+        
+    rs = np.array(rs)
+    
+    mean_r = np.nanmean(rs, axis=0) # mean across k folds 
+
+    return mean_r
+
 def word2sense(embedding_file):
     f = open(embedding_file, 'r', encoding='utf-8')
     temp = f.readlines()
-    wordlist = [line.split(',')[0] for idx, line in enumerate(temp) if idx != 0]
-    nDims = 2250
+    wordlist = [line.split(',')[0] for idx, line in enumerate(temp) if idx != 0] # create word list
     f.close()
-    
-    word2sense = {new_list: [] for new_list in wordlist} # create word embedding dictionary
+    word2sense = {new_list: [] for new_list in wordlist} # create (word: embedding) dictionary
     for i, line in enumerate(temp):
         if i == 0:
             continue
@@ -30,7 +68,7 @@ def word2sense(embedding_file):
         embedding_float = ([float(j) for j in embedding[1:]])
         word2sense[embedding[0]] = embedding_float
     word2sense = OrderedDict(word2sense)
-    return wordlist, nDims, word2sense
+    return wordlist, word2sense
 
 def listdir(dir, path=True):
     files = os.listdir(dir)
@@ -126,29 +164,29 @@ def cv_regression(condition_features, subject, l2=0.0):
     return weights, mean_r
 
 
-def cv_regression_w2s(condition_Things, w2s, l2=0.0):
-    # Get cross-validated mean test set correlation
-    rs = []
+# def cv_regression_w2s(condition_Things, w2s, l2=0.0):
+#     # Get cross-validated mean test set correlation
+#     rs = []
     
-    for test_conditions in subject.cv_sets:
-        train_conditions = [c for c in subject.conditions if c not in test_conditions]
-        train_features = np.stack([condition_features[c] for c in train_conditions])
-        test_features = np.stack([condition_features[c] for c in test_conditions])
-        train_voxels = np.stack([subject.condition_voxels[c] for c in train_conditions])
-        test_voxels = np.stack([subject.condition_voxels[c] for c in test_conditions])
-        #_, r = L1_regression(train_features, train_voxels, test_features, test_voxels, l2=l2)
-        _, r = regression(train_features, train_voxels, test_features, test_voxels, l2=l2)
-        rs.append(r)
-    mean_r = np.mean(rs)
+#     for test_conditions in subject.cv_sets:
+#         train_conditions = [c for c in subject.conditions if c not in test_conditions]
+#         train_features = np.stack([condition_features[c] for c in train_conditions])
+#         test_features = np.stack([condition_features[c] for c in test_conditions])
+#         train_voxels = np.stack([subject.condition_voxels[c] for c in train_conditions])
+#         test_voxels = np.stack([subject.condition_voxels[c] for c in test_conditions])
+#         #_, r = L1_regression(train_features, train_voxels, test_features, test_voxels, l2=l2)
+#         _, r = regression(train_features, train_voxels, test_features, test_voxels, l2=l2)
+#         rs.append(r)
+#     mean_r = np.mean(rs)
 
-    # Train on all of the data
-    train_conditions = subject.conditions
-    train_features = np.stack([condition_features[c] for c in train_conditions])
-    train_voxels = np.stack([subject.condition_voxels[c] for c in train_conditions])
-    weights = regression(train_features, train_voxels, None, None, l2=l2, validate=False)
-    #_, r = L1_regression(train_features, train_voxels, test_features, test_voxels, l2=l2)
+#     # Train on all of the data
+#     train_conditions = subject.conditions
+#     train_features = np.stack([condition_features[c] for c in train_conditions])
+#     train_voxels = np.stack([subject.condition_voxels[c] for c in train_conditions])
+#     weights = regression(train_features, train_voxels, None, None, l2=l2, validate=False)
+#     #_, r = L1_regression(train_features, train_voxels, test_features, test_voxels, l2=l2)
     
-    return weights, mean_r
+#     return weights, mean_r
 
 
 def regression(x_train, y_train, x_test, y_test, l2=0.0, validate=True):
